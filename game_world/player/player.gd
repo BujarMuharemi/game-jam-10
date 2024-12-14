a1f7c6fecb121e7f86ceb88f3eacef9407cd2043
Kevin <Kevin.Kraus271@gmail.com>
2024-12-14 20:52:05 +0100

    change player with correct model :3

--- game_world/player/player.gd
+++ game_world/player/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
+@onready var player_rig: Node2D = $player_rig
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -700.0
 
 
 func _physics_process(delta: float) -> void:
+	var animationPlayer = player_rig.get_node("AnimationPlayer")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -17,6 +20,22 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+	
+	# Toggle movement direction animation
+	if direction > 0:
+		player_rig.scale.x = 1  # Rechts
+	elif direction < 0:
+		player_rig.scale.x = -1  # Links
+	
+	# play animations
+	if is_on_floor():
+		if direction == 0:
+			animationPlayer.play("idle")
+		else:
+			animationPlayer.play("walk")
+	else:
+		animationPlayer.play("jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
