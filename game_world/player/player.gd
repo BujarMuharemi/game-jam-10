a75526ec6e0e8c933e4cee5598098bbe219c3cda
Kevin <Kevin.Kraus271@gmail.com>
2024-12-15 11:58:36 +0100

    add player scripts

--- game_world/player/player.gd
+++ game_world/player/player.gd
@@ -2,9 +2,15 @@ extends CharacterBody2D
 
 @onready var player_rig: Node2D = $player_rig
 
-const SPEED = 500.0
+const BASE_SPEED = 500.0
 const JUMP_VELOCITY = -700.0
+var SPEED = BASE_SPEED  # Standardgeschwindigkeit
+var current_jump_velocity = JUMP_VELOCITY
 
+var active_effects := {}
+var drug_timer = 0
+var drug_activated_flag=false
+var drug_durataion = 0
 
 func _physics_process(delta: float) -> void:
 	var animationPlayer = player_rig.get_node("AnimationPlayer")
@@ -47,3 +53,47 @@ func _physics_process(delta: float) -> void:
 	#for i in get_slide_collision_count():
 	#	var collision = get_slide_collision(i)
 		#print("I collided with ", collision.get_collider().name)
+		
+		
+func reset_speed():
+	SPEED = BASE_SPEED
+	drug_timer = 0
+	print("Speed zurückgesetzt auf:", SPEED)
+	drug_activated_flag = true
+
+func apply_drug_effects(effects: Dictionary, duration: float):
+	for effect in effects:
+		match effect:
+			"speed_multiplier":
+				SPEED = BASE_SPEED * effects["speed_multiplier"]
+			"jump_boost":
+				current_jump_velocity = JUMP_VELOCITY * effects["jump_boost"]
+			"reaction_time":
+				print("Reaktionszeit verändert um:", effects["reaction_time"])
+				# Hier könntest du die Reaktionszeit entsprechend anpassen
+
+	active_effects = effects
+	drug_timer = duration
+	print("Neue Effekte angewendet:", effects, "für", duration, "Sekunden")
+
+func set_speed(speed,duration):
+	SPEED = speed * BASE_SPEED
+	drug_durataion = duration
+	drug_activated_flag=true
+
+func consume_pickup(pickup: PickUp):
+	pickup.apply_effect(self)
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("pickup"):
+		consume_pickup(area.get_parent())
+
+func _process(delta: float) -> void:
+	if drug_activated_flag:
+		drug_timer+= delta
+		print(drug_timer,"----",drug_durataion)
+
+		if drug_timer > drug_durataion:
+			SPEED = BASE_SPEED
+			drug_activated_flag=false
+			drug_timer=0
